dd0fccdb1c97b5e5d7a151f03a3434d071f6f8ba
Roman <venom1994@inbox.ru>
2023-07-26 13:28:51 +0300

    files changed

--- app.py
+++ app.py
@@ -64,14 +64,6 @@ def success():
                 img = filename
                 # class_result , prob_result = predict(img_path , model)
                 class_result = predict(model, img_path, img_size=224)
-                # predictions = {
-                #     "class1":class_result[0],
-                #     "class2":class_result[1],
-                #     "class3":class_result[2],
-                #     "prob1": prob_result[0],
-                #     "prob2": prob_result[1],
-                #     "prob3": prob_result[2],
-                # }
                 predictions = {
                     "class1":class_result,
                     "prob1": 1
@@ -92,14 +84,6 @@ def success():
                 img = file.filename
                 # class_result, prob_result = predict(img_path, model)
                 class_result = predict(model, img_path, img_size=224)
-                # predictions = {
-                #     "class1":class_result[0],
-                #     "class2":class_result[1],
-                #     "class3":class_result[2],
-                #     "prob1": prob_result[0],
-                #     "prob2": prob_result[1],
-                #     "prob3": prob_result[2],
-                # }
                 predictions = {
                     "class1":class_result,
                     "prob1": 1
@@ -109,7 +93,7 @@ def success():
                 error = "Please upload images of jpg , jpeg and png extension only"
 
             if len(error) == 0:
-                return  render_template('success.html', img=img, predictions=predictions)
+                return render_template('success.html', img=img, predictions=predictions)
             else:
                 return render_template('index.html', error=error)
     else:
