9fe14803dc9a19181897da55706fcd4c33fbbac3
Roman <venom1994@inbox.ru>
2023-07-29 00:42:45 +0300

    Dockerfile changed. All files are copied into workspace.

--- app/app.py
+++ app/app.py
@@ -8,14 +8,14 @@ import torchvision.transforms as transforms
 from flask import Flask, render_template, request
 
 
-def get_model(model_dir='models/my_model'):
+def get_model(model_dir):
     model = torch_models.shufflenet_v2_x1_5(weights=None)
     model.load_state_dict(torch.load(model_dir))
     model.eval()
     return model
 
 
-def predict(model, img_path, img_size=224):
+def predict(model, img_path, img_size):
     class_to_classname = ["fresh", "rotten"]
     # og_transform = transforms.Compose([transforms.Resize((img_size, img_size)),
     #                                    transforms.ToTensor()])
@@ -51,7 +51,7 @@ def prediction():
         filename = file.filename
         img_path = os.path.join('./static', filename)
         file.save(img_path)
-        model = get_model(model_dir='models/my_model')
+        model = get_model(model_dir='./models/my_model')
         pred = predict(model=model, img_path=img_path, img_size=224)
         print(pred)
 
